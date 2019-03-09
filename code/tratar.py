efd2707fd510994a918ef1c1d969d784b8cf8e39
Estela Balboa <estela.balboa@gmail.com>
2019-03-09 11:50:12 +0100

    feat: Añadido try para evitar problemas de certificados ssl

--- code/tratar.py
+++ code/tratar.py
@@ -1,32 +1,55 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 28 13:16:31 2018
-
-@author: david
-"""
  
-import re   
-from sklearn.datasets import load_files 
-from sklearn.feature_extraction.text import TfidfVectorizer  
-from sklearn.model_selection import train_test_split  
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-#nltk.download('stopwords')    
-from nltk.corpus import stopwords
+import os
+import re
+import ssl
 
-movie_data = load_files(r"./../texts")  
-X, y = movie_data.data, movie_data.target  
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.datasets import load_files
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+
+# Todo: Not use absolute paths!
+os.chdir("/Users/ebalboa/Documents/AI-SATURDAYS/20190309_nlp/2016-campaign/")
+
+# movie_data = load_files(r"./../texts")
+
+speech_data = load_files(r"texts/")
+# 'target_names': ['Clinton', 'Trump']
+
+X, y = speech_data.data, speech_data.target
+# speech_data.target gives an array of zeros and ones.
+print(y.__len__())
 
 documents = []
 
-from nltk.stem import WordNetLemmatizer
-#nltk.download('wordnet')
+try:
+    """
+    Try is used for mac users that face with a ssl certificate issue, to let them download from nltk library
+    """
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+# nltk.download()
+
+
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+
 stemmer = WordNetLemmatizer()
 
-for sen in range(0, len(X)):  
+print("Stopwords and wordnet downloaded successful")
+
+for sen in range(0, len(X)):
     
     # Remove all the special characters
-    document = re.sub(r'\W', ' ', str(X[sen]))
+    document: str = re.sub(r'\W', ' ', str(X[sen]))
 
     # remove all single characters
     document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
@@ -40,7 +63,7 @@ for sen in range(0, len(X)):
     # Removing prefixed 'b'
     document = re.sub(r'^b\s+', '', document)
     
-      # Removing unwanted characters
+    # Removing unwanted characters
     document = re.sub(r'x9[0-9]', '', document)
 
     # Converting to Lowercase
@@ -66,12 +89,10 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test) 
 
 
+print("Confusion Matrix: ")
+print(confusion_matrix(y_test,y_pred))
 
-print(confusion_matrix(y_test,y_pred))  
-print(classification_report(y_test,y_pred))  
-print(accuracy_score(y_test, y_pred)) 
+print("Classification Report:c")
+print(classification_report(y_test,y_pred))
 
-
- 
- 
-
+print("Accuracy Score achieved: " + str(accuracy_score(y_test, y_pred)))
